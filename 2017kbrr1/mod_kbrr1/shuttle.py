072ff83842477ed07e23fd0bd2851ae9c49ab705
Deokgon Kim <dgkim@dgkim.net>
2017-10-01 15:52:05 +0900

    카카오 신입 공채 1차 코딩 테스트 4번 문제 심심풀이중.

--- 2017kbrr1/mod_kbrr1/shuttle.py
+++ 2017kbrr1/mod_kbrr1/shuttle.py
@@ -1,8 +1,32 @@
 #! -*- coding=utf-8 -*-
 
+from datetime import timedelta
+
+
+def str_to_timedelta(str_time):
+    """'HH:mm' 형태의 시간을 datetime.timedelta 형식으로 변환한다.
+    """
+    h, m = str_time.split(':')
+    return timedelta(0, 3600 * int(h) + 60 * int(m))
+
 
 class ShuttleBus:
-    pass
+    """Shuttle bus time table을 관리한다.
+    """
+    def __init__(self, start_time="09:00", n = 1, t = 1):
+        self.time_table = []
+        d = str_to_timedelta(start_time)
+        self.time_table.append(d)
+        for i in range(1, n):
+            d += str_to_timedelta('00:' + str(t))
+            self.time_table.append(d)
+
+    def print_time_table(self):
+        """셔틀 버스 시간표를 출력한다.
+        """
+        #print(self.time_table)
+        for i, t in enumerate(self.time_table):
+            print("%d : %s" % (i+1, t))
 
 class BusStop:
     pass
---
+++ 2017kbrr1/test4.py
@@ -0,0 +1,30 @@
+#! -*- coding=utf-8 -*-
+
+"""
+Sample data and result
+n	t	m	timetable				answer
+1 	1 	5 	["08:00", "08:01", "08:02", "08:03"] 	"09:00"
+2 	10 	2 	["09:10", "09:09", "08:00"] 	"09:09"
+2 	1 	2 	["09:00", "09:00", "09:00", "09:00"] 	"08:59"
+1 	1 	5 	["00:01", "00:01", "00:01", "00:01", "00:01"] 	"00:00"
+1 	1 	1 	["23:59"] 	"09:00"
+10 	60 	45 	["23:59","23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59", "23:59"] 	"18:00"
+"""
+
+n = input("Enter count of shuttle bus? ")
+t = input("Enter time interval of shuttle bus? ")
+m = input("Enter vacancy count of shuttle bus? ")
+
+timetable = input("Enter crew's arrival timetable(Valid Phthon Expression)? ")
+
+assert 0 < n <= 10, "Shuttle Bus count exceeds limit(1~10)"
+assert 0 < t <= 60, "Shuttle Bus time interval is invalid(1~60)"
+assert 0 < m <= 45, "Shuttle Bus vacancy count is invalid(1~45)"
+
+assert 0 < len(timetable) <= 2000, "Crew count is invalid(1~2000)"
+
+from mod_kbrr1.shuttle import *
+
+shuttle_bus = ShuttleBus("09:00", n, t)
+
+shuttle_bus.print_time_table()
