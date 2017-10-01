1ae3fa47bd47f6b853a3d88290f4cc54222f0345
Deokgon Kim <dgkim@dgkim.net>
2017-10-01 16:39:05 +0900

    구현하다가, ... 내일 또 할려고, 남겨둠.

--- 2017kbrr1/mod_kbrr1/shuttle.py
+++ 2017kbrr1/mod_kbrr1/shuttle.py
@@ -1,6 +1,7 @@
 #! -*- coding=utf-8 -*-
 
 from datetime import timedelta
+import logging
 
 
 def str_to_timedelta(str_time):
@@ -10,24 +11,64 @@ def str_to_timedelta(str_time):
     return timedelta(0, 3600 * int(h) + 60 * int(m))
 
 
-class ShuttleBus:
-    """Shuttle bus time table을 관리한다.
+class BusSupplier:
+    """Shuttle bus를 생산한다?
     """
-    def __init__(self, start_time="09:00", n = 1, t = 1):
-        self.time_table = []
+    def __init__(self, default_vacancy = 45):
+        self.vacancy = default_vacancy
+
+    def get_bus(self):
+        """버스를 생산한다.
+        """
+        bus = ShuttleBus(self.vacancy)
+        return bus
+
+
+class ShuttleBus:
+    def __init__(self, vacancy):
+        self.vacancy = vacancy
+
+
+class BusAgency:
+    """버스 배차 및 탑승을 관장하는 기관?
+    """
+    def __init__(self):
+        self.bus_supplier = None
+
+        self.bus_list = []
+        self.passenger_list = []
+        self.logger = logging.getLogger("kbrr1")
+        self.logger.setLevel("INFO")
+
+    def set_bus_supplier(self, supplier):
+        self.bus_supplier = supplier
+
+    def schedule_bus(self, start_time="09:00", n = 1, t = 1):
+        """
+        """
+        if not self.bus_supplier: raise Exception("There is no Bus supplier")
         d = str_to_timedelta(start_time)
-        self.time_table.append(d)
+        self.bus_list.append(dict((("time", d),("bus", "hi"))))
         for i in range(1, n):
             d += str_to_timedelta('00:' + str(t))
-            self.time_table.append(d)
+            self.bus_list.append(dict((("time", d),("bus", "hi" + str(i)))))
 
-    def print_time_table(self):
+    def guess_last_bus(self):
+        """주어진, 버스 시간표와, 탑승객 명단을 가지고,
+        마지막 버스를 탑승할 수 있는 시각을 알려준다.
+        """
+        pass
+
+    def _process_shuttle_bus(self):
+        """셔틀 버스를 운영하면서, 현황을 출력한다.
+        """
+        for i, bus in enumerate(self.bus_list):
+            self.logger.info("Bus %d Alived %s" % (i+1, bus))
+        pass
+
+    def _print_bus_time_table(self):
         """셔틀 버스 시간표를 출력한다.
         """
-        #print(self.time_table)
-        for i, t in enumerate(self.time_table):
-            print("%d : %s" % (i+1, t))
-
-class BusStop:
-    pass
-
+        for d in self.bus_list:
+            #print("%s : %s" % (d['time'], d['bus']))
+            print(d)
